d9d3b1a43dc9a35d9388db0aa64ac86e7861b5fe
AxFerry <162710384+AxFerry@users.noreply.github.com>
2024-11-17 11:41:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const db = new sqlite.Database('./message.db', sqlite.OPEN_READwRITE,(err) =>{
     })
 
 function sha256(content){
-    return crypto.createHash('sha256').update(content).digest('hex');
+    if(content != null){return crypto.createHash('sha256').update(content).digest('hex');}
+    else return 'abcd'
 }
 
 
