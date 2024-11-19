cce8f4ad3648011275c8913813e7cc498f35561f
AxFerry <162710384+AxFerry@users.noreply.github.com>
2024-11-19 20:17:43 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const express = require('express');
 const bodyparser = require('body-parser');
 const url = require('url');
